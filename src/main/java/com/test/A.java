0ac228d82dfbc27ca85e213c01004f86d27b6307
luning110 <ln951984641@163.com>
2017-10-06 10:01:02 +0800

    add get and set

--- src/main/java/com/test/A.java
+++ src/main/java/com/test/A.java
@@ -3,4 +3,17 @@ package com.test;
 public class A {
    private int num;
    private String str;
+public int getNum() {
+	return num;
+}
+public void setNum(int num) {
+	this.num = num;
+}
+public String getStr() {
+	return str;
+}
+public void setStr(String str) {
+	this.str = str;
+}
+   
 }
