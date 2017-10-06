da45a0fd2555781f57ff82ddc33591eff4361856
luning110 <ln951984641@163.com>
2017-10-06 10:40:10 +0800

    delete get and set

--- src/main/java/com/test/A.java
+++ src/main/java/com/test/A.java
@@ -3,17 +3,5 @@ package com.test;
 public class A {
    private int num;
    private String str;
-public int getNum() {
-	return num;
-}
-public void setNum(int num) {
-	this.num = num;
-}
-public String getStr() {
-	return str;
-}
-public void setStr(String str) {
-	this.str = str;
-}
    
 }
