a55c6b4de2b6a3150c175074e929ba8638e2c7c9
ninglu3 <ninglu3@creditease.cn>
2018-02-08 15:06:13 +0800

    4444444

--- src/main/java/com/test/A.java
+++ src/main/java/com/test/A.java
@@ -3,11 +3,6 @@ package com.test;
 public class A {
    private int num1;
    private String str = "String_master_1112222";
- 
-   private double num2;
-   
-   private boolean bol = false;
 
-   private String s = "abc";
 
 }
